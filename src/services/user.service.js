590e582e4c1400141fed0423b52e0c18a60a9197
Matheus Oliveira <matheusoliveira09222004@gmail.com>
2025-09-21 00:08:36 -0300

    🐛 fixed issue related to data catching

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -8,6 +8,7 @@ const SELECT_USER_WITHOUT_PASSWORD = {
   "email": true,
   "age": true,
   "gender": true,
+  "bmi": true,
   "weight": true,
   "height": true,
   "objective": true,
@@ -37,7 +38,8 @@ export async function updateUser(id, data) {
   }
 
   if (data.weight || data.height) {
-    calculateBmi(id,data.weight,data.height,)
+    const { bmi, bmi_category } = await calculateBmi(id,data.weight,data.height,)
+    data.bmi = bmi
   }
 
   return await prisma.users.update({
@@ -56,8 +58,10 @@ export async function deleteUser(id) {
 async function calculateBmi(id, paramWeight, paramHeight) {
   const hasParams = Boolean(paramHeight && paramWeight);
 
+  let user = {};
+
   if (!hasParams) {
-    const user = await prisma.users.findUnique({
+    user = await prisma.users.findUnique({
       where: { user_id: Number(id) },
       select: { weight: true, height: true }
     });
@@ -71,27 +75,30 @@ async function calculateBmi(id, paramWeight, paramHeight) {
     }
   }
 
-  const weight = paramWeight ?? user.weight;
-  const height = paramHeight ?? user.height;
+  let weight = paramWeight ?? user.weight;
+  let height = paramHeight ?? user.height;
+
+  weight = Number(weight);
+  height = Number(height);
 
   const heightInMeters = height / 100;
   const bmi = weight / (heightInMeters * heightInMeters);
 
-  let category = "";
+  let bmi_category = "";
 
   if (bmi < 18.5) {
-    category = "Underweight";
+    bmi_category = "Underweight";
   } else if (bmi < 25) {
-    category = "Normal weight";
+    bmi_category = "Normal weight";
   } else if (bmi < 30) {
-    category = "Overweight";
+    bmi_category = "Overweight";
   } else {
-    category = "Obesity";
+    bmi_category = "Obesity";
   }
 
   return {
     bmi: Number(bmi.toFixed(2)),
-    category
+    bmi_category
   };
 }
 
