4ae486971bc30282c6f7bc4116384e0a80643653
Rodrigo França <rodrigo.anacleto84@gmail.com>
2025-09-27 16:28:13 -0300

    ✨ add route to validate token directly

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -5,6 +5,7 @@ import {
   requestPasswordReset,
   resetPassword
 } from "../controllers/auth.controller.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = Router();
 
@@ -12,5 +13,8 @@ router.post("/register", register);
 router.post("/login", login);
 router.post("/requestPasswordReset", requestPasswordReset);
 router.put("/resetPassword", resetPassword)
+router.post("/validateToken", authMiddleware, (req, res) => {
+  res.json({valid: true, user: req.user})
+})
 
 export default router;
