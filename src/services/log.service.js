36605a06885e040098cf37273cbbe84d23491b36
Matheus Oliveira <matheusoliveira09222004@gmail.com>
2025-11-26 19:02:34 -0300

    ♻️ removed the following crud  functions: delete, update, list AND updated the create function to match the new structure

--- src/services/log.service.js
+++ src/services/log.service.js
@@ -1,32 +1,13 @@
 import { prisma } from "../config/prisma.js";
 
-export async function createLog(data) {
-  return await prisma.logs.create({
-    data,
-  });
-}
-
-export async function listLogs() {
-  return await prisma.logs.findMany({
-    orderBy: { created_at: 'desc' },
-  });
-}
-
-export async function getLog(id) {
-  return await prisma.logs.findUnique({
-    where: { log_id: Number(id) },
-  });
-}
-
-export async function updateLog(id, data) {
-  return await prisma.logs.update({
-    where: { log_id: Number(id) },
-    data,
-  });
-}
-
-export async function deleteLog(id) {
-  return await prisma.logs.delete({
-    where: { log_id: Number(id) },
-  });
+export async function createLog({user_id, message, entity_type, entity_id, action }) {
+    return await prisma.logs.create({
+        data: {
+            user_id,
+            message,
+            entity_type,
+            entity_id,
+            action,
+        },
+    });
 }
