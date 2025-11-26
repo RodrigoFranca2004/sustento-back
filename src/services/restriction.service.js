9eb2526325d9154413f44f55e0e25889c9a99064
Matheus Oliveira <matheusoliveira09222004@gmail.com>
2025-11-26 19:05:28 -0300

    ✨added the log insertion logic to the necessesary functions

--- src/services/restriction.service.js
+++ src/services/restriction.service.js
@@ -1,6 +1,12 @@
 import { prisma } from "../config/prisma.js";
+import { createLog } from "./log.service.js";
 
 export async function createRestriction(data) {
+  await createLog({
+      message: "ALLOWED RESTRICTIONS UPDATED",
+      entity_type: "RESTRICTION",
+      action: "CREATE"
+    });
   return await prisma.restrictions.create({
     data: {
       ...data,
@@ -19,6 +25,12 @@ export async function getRestriction(id) {
 }
 
 export async function updateRestriction(id, data) {
+    await createLog({
+      message: "ALLOWED RESTRICTIONS UPDATED",
+      entity_type: "RESTRICTION",
+      entity_id: id,
+      action: "UPDATE"
+    });
     return await prisma.restrictions.update({
         where: { restriction_id: Number(id) },
         data,
@@ -26,6 +38,12 @@ export async function updateRestriction(id, data) {
 }
 
 export async function deleteRestriction(id) {
+  await createLog({
+      message: "ALLOWED RESTRICTIONS UPDATED",
+      entity_type: "RESTRICTION",
+      entity_id: id,
+      action: "DELETE"
+    });
   return await prisma.restrictions.delete({
     where: { restriction_id: Number(id) },
   });
