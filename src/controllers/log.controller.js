d1bf581755ad7d7896d9159b6e06d3ba2fa2424c
Matheus Oliveira <matheusoliveira09222004@gmail.com>
2025-11-26 19:00:22 -0300

    ♻️ removed the following crud functions: update, delete, list AND improved the create function to match the new structure

--- src/controllers/log.controller.js
+++ src/controllers/log.controller.js
@@ -2,51 +2,9 @@ import * as LogService from "../services/log.service.js";
 
 export async function createLog(req, res) {
   try {
-    const log = await LogService.createLog(req.body);
+    const { user_id, message  } = req.body;
+    const log = await LogService.createLog({user_id, message});
     res.status(201).json(log);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-}
-
-export async function listLogs(req, res) {
-  try {
-    const logs = await LogService.listLogs();
-    res.json(logs);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-}
-
-export async function getLog(req, res) {
-  try {
-    const logId = req.params.id;
-    const log = await LogService.getLog(logId);
-    if (!log) {
-      return res.status(404).json({ error: "Log not found" });
-    }
-    res.json(log);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-}
-
-export async function updateLog(req, res) {
-  try {
-    const logId = req.params.id;
-    const data = req.body;
-    const log = await LogService.updateLog(logId, data);
-    res.json(log);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-}
-
-export async function deleteLog(req, res) {
-  try {
-    const logId = req.params.id;
-    await LogService.deleteLog(logId);
-    res.status(204).send();
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
--- src/routes/log.routes.js
+++ src/routes/log.routes.js
@@ -1,12 +1,8 @@
 import { Router } from "express";
-import { createLog, listLogs, getLog, updateLog, deleteLog } from "../controllers/log.controller.js";
+import { createLog } from "../controllers/log.controller.js";
 
 const router = Router();
 
 router.post("/", createLog);
-router.get("/", listLogs);
-router.get("/:id", getLog);
-router.put("/:id", updateLog);
-router.delete("/:id", deleteLog);
 
 export default router;
